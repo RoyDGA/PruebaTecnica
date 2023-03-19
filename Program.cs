ca8b550bd9d01f5e6a5e4fc833dc682bb2535637
RoyDGA <108435522+RoyDGA@users.noreply.github.com>
2023-03-19 18:37:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿String elegir;
+do {
+    Console.WriteLine("(1)Area del Circulo || (2)Area del rectangulo ||(3)Salir del programa");
+
+    Console.WriteLine("Ingrese un numero de acuerdo a la función: ");
+    elegir = Console.ReadLine();
+
+    if (elegir == "1") {
+
+        Console.Write("Radio del circulo: ");
+        double radio = Convert.ToDouble(Console.ReadLine());
+        double area = Math.PI * Math.Pow(radio, 2);
+
+        Console.WriteLine($"El area del circulo es: {area}");
+
+        } else if (elegir == "2") {
+            
+            Console.Write("Base del rectángulo: ");
+            double baseR = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Altura del rectángulo: ");
+            double alturaR = Convert.ToDouble(Console.ReadLine());
+            
+            double area = baseR * alturaR;
+            Console.WriteLine($"El area del rectángulo es: {area}");
+            } else if (elegir != "3") {
+                Console.WriteLine("Numero fuera de rango");
+                }
+                Console.WriteLine();
+                } while (elegir != "3");
+                
+                Console.WriteLine("Programa terminado");
